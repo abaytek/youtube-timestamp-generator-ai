dca476ee4d380d00ec08de509c04e7156fce788d
abaytek <abaysewtekle@gmail.com>
2024-01-04 16:09:17 +0000

    fix: google ai API key fix

--- services/APIService.tsx
+++ services/APIService.tsx
@@ -4,36 +4,32 @@ class ApiService {
   private api: AxiosInstance;
 
   constructor(baseURL: string) {
-    // Set the base URL for the API
     this.api = axios.create({
       baseURL: baseURL,
     });
   }
 
-  // GET method
   async get(endpoint: string, params = {}) {
     try {
-        console.log(endpoint)
+      console.log(endpoint);
       const response = await this.api.get(endpoint, { params });
       return response.data;
     } catch (error: any) {
-      console.error('GET Request Error:', error.message);
+      console.error("GET Request Error:", error.message);
       throw error;
     }
   }
 
-  // POST method
   async post(endpoint: string, data = {}) {
     try {
       const response = await this.api.post(endpoint, data);
       return response.data;
     } catch (error: any) {
-      console.error('POST Request Error:', error.message);
+      console.error("POST Request Error:", error.message);
       throw error;
     }
   }
 }
 
 // Example usage:
-export const APIService = new ApiService('http://localhost:3000/api');
-
+export const APIService = new ApiService("http://localhost:3000/api");
